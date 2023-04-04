76784ec81b2906ace39409ab1cd64164e2256ade
Courtney McKown <wheelerzac@yahoo.com>
2023-04-04 17:21:28 -0500

    added javascript comments

--- script.js
+++ script.js
@@ -5,6 +5,20 @@ var uppercaseChars = "ABCDEFGHIFGHIJKLMNOPQRSTUVWXYZ";
 var numericalChars = "0123456789";
 var specialChars = "!#$%&()*+,-./:;<=>?@[]^_{|}~";
 
+function generatePassword() {
+  console.log("You've clicked the button!");
+// 1. prompt user for password guidelines.
+//   a. password length 8 < 128
+//   b. Uppercase, Lowercase, and special characters     
+// 2. Verify the input(s).
+// 3. Generate accepted password.
+
+
+
+// 4. Display password on page.
+  return "Generated password will go here.";
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
