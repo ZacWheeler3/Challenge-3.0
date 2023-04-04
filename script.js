268b8f35a7e654fe4cc314a14b997c6c8fdae80a
Courtney McKown <wheelerzac@yahoo.com>
2023-04-04 18:34:49 -0500

    added var arrays

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChars = "ABCDEFGHIFGHIJKLMNOPQRSTUVWXYZ";
-var numericalChars = "0123456789";
-var specialChars = "!#$%&()*+,-./:;<=>?@[]^_{|}~";
+var lowercaseChars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
+var uppercaseChars = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+var numericalChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',];
+var specialChars = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', ':', ';', '<', '>', '?', '@', '[', ']', '~',]
 
 function generatePassword() {
+  var password = []
+  Math.random()
   console.log("You've clicked the button!");
 // 1. prompt user for password guidelines.
 //   a. password length 8 < 128
